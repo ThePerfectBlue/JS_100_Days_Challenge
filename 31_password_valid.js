44ef20378ad60faea30d2de3e50fcdac7d5e0d29
Ravi Kumar <77407433+ThePerfectBlue@users.noreply.github.com>
2024-04-26 13:54:23 +0000

    day 31 done

---
+++ 31_password_valid.js
@@ -0,0 +1,17 @@
+function passwordValid(pass){
+    if(pass.length < 8) return false;
+
+    let hasLowerCase = false;
+    let hasUpperCase = false;
+    let hasDigit = false;
+
+    for(let char of pass){
+        if(char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90)    hasLowerCase = true;
+        if(char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122)    hasUpperCase = true;
+        if(char.charCodeAt(0) >= 48 && char.charCodeAt(0) <= 57)    hasDigit = true;
+    }
+    return (hasLowerCase && hasUpperCase && hasDigit) ? true : false;
+
+}
+
+console.log(passwordValid("123a"));
